2b9ef5b968a9947c075453257aa0c6b9c231e76f
Antoine Gourgue <antoine.gourgue@epitech.eu>
2025-05-12 10:32:20 +0200

    feat: cnn to 256

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -3,16 +3,14 @@ import os
 import cv2
 import tensorflow as tf
 
-# Charger le modèle entraîné
 model = tf.keras.models.load_model("models/cnn_pneumonia.h5")
 
-# Fonction de prédiction sur une nouvelle image
 def predict_pneumonia(image_path):
     IMG_SIZE = (64, 64)
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, IMG_SIZE)
     img = img / 255.0
-    img = img.reshape(-1, 64, 64, 1)  # Format pour le CNN
+    img = img.reshape(-1, 64, 64, 1)
 
     prediction = model.predict(img)[0][0]
     if prediction > 0.5:
@@ -20,7 +18,5 @@ def predict_pneumonia(image_path):
     else:
         return "NORMAL lungs ✅"
 
-# Exemple d'utilisation
-#image_path = "data/chest_xray/test/NORMAL/IM-0001.jpeg"  # Remplace avec une image test
 image_path = os.path.join(os.path.dirname(__file__), "../../data/chest_xray/test/NORMAL/IM-0001-0001.jpeg")
 print(predict_pneumonia(image_path))
